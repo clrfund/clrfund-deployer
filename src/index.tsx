9a3b70747743679c114b2c95ec3d87bfe867fc40
daodesigner <designerdao.eth@gmail.com>
2021-04-12 23:17:05 -0400

    dev testnet

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
     <ColorModeScript initialColorMode="dark" />
-    <Web3Provider networkId={100}>
+    <Web3Provider networkId={1337}>
       <Router>
         <App />
       </Router>
